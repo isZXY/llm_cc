76e6df6e121a2e54dcf707c53202befbea7839c0
isZXY <xuanyu0827@gmail.com>
2024-09-28 23:10:34 +0800

    1

--- src/llmcc_model.py
+++ src/llmcc_model.py
@@ -6,12 +6,12 @@ class _CategoryProjection(nn.Module):
     def __init__(self,hidden_size,num_classes):
         super(_CategoryProjection, self).__init__()
         self.classification_head = nn.Linear(hidden_size, num_classes)  # 线性层映射到分类标签
-        self.softmax = nn.Softmax(dim=-1)  # 用于生成分类概率
+        # self.softmax = nn.Softmax(dim=-1)  # 用于生成分类概率
 
     def forward(self,llm_last_hidden_state):
-        cls_token_hidden_state = llm_last_hidden_state[:, 0, :]  # (batch_size, hidden_size)
+        cls_token_hidden_state = llm_last_hidden_state[:, -1:, :]  # (batch_size, hidden_size)
         logits = self.classification_head(cls_token_hidden_state)  # (batch_size, num_classes)
-        return self.softmax(logits)
+        return logits
 
 class Model(nn.Module):
     def __init__(self,plm_path,device,num_classes):
@@ -20,13 +20,11 @@ class Model(nn.Module):
         self.device = device
         for param in self.llm_model.parameters():
             param.requires_grad = False # 冻结大模型的参数
-    
         self.input_embedding_layer = DatasetEmbedding(self.tokenizer, self.llm_model,self.device)
         self.output_projection = _CategoryProjection(self.llm_model.config.hidden_size,num_classes)
 
-    def forward(self, batch_prompt, batch_ts, batch_label):
+    def forward(self, batch_prompt, batch_ts):
         input_embedding = self.input_embedding_layer(batch_prompt,batch_ts)
         llm_last_hidden_state = self.llm_model(input_embedding).last_hidden_state # (batch size, sequence length, hidden size)
         output_category = self.output_projection(llm_last_hidden_state)
-
         return output_category
--- src/run.py
+++ src/run.py
@@ -1,11 +1,12 @@
 import torch
 from torch.utils.tensorboard import SummaryWriter
 from torch import nn
+import torch.optim as optim
 
 from dataset_loader import DatasetLoader
 from dataset_input_embedding import DatasetEmbedding
 from pretrained_model import PretrainedLanguageModel
-
+from llmcc_model import Model
 
 from tqdm import tqdm
 
@@ -14,30 +15,30 @@ if __name__ == "__main__":
     device = torch.device("cuda:2")
     plm_path = "../llama-7b"
     boardwriter = SummaryWriter(log_dir='logs')
+    num_classes = 7
 
     # 1. load dataset
     train_loader,val_loader,test_loader = DatasetLoader('../datasets/dataset_pool.pkl')
 
-    # 2. Load Model,Tokenizer
-    pretrained_llm = PretrainedLanguageModel(model_name='llama',model_path=plm_path,device=device)
+    # 2. Load Model
+    model = Model(plm_path,device,num_classes)
 
-    # 3. Tokenize &  Embedding
-    embedder = DatasetEmbedding(pretrained_llm)
 
-    # 4. train(forward & backward)
+
+    # 3. Set train paras
+    learning_rate = .01
+    train_epochs = 1
     # checkpoint_save_path = ()
     # train_steps = len(train_loader)
-    # 
-    # for p in model.parameters():
-    #     if p.requires_grad is True:
-    #         trained_parameters.append(p)
+    trained_parameters = []
+    for p in model.parameters():
+        if p.requires_grad is True:
+            trained_parameters.append(p)
 
-    # model_optim = optim.Adam(trained_parameters, lr=args.learning_rate) # 优化器
+    optimizer = optim.Adam(trained_parameters, lr=learning_rate) # 优化器
+    loss_fcn = nn.CrossEntropyLoss()
 
     # 迭代加载数据
-
-    criterion = nn.CrossEntropyLoss()
-    train_epochs = 1
     for epoch in range(train_epochs):
         tot_iter_count = 0
         train_loss = []
@@ -45,7 +46,12 @@ if __name__ == "__main__":
         for i, (batch_prompt, batch_ts, batch_label) in tqdm(enumerate(train_loader)):
             tot_iter_count+=1
             # model_optim.zero_grad()
+            optimizer.zero_grad()
             
+            logits = model(batch_prompt, batch_ts)
             
-            
+            loss = loss_fcn(logits, batch_label)
+            train_loss.append(loss.item())
+            loss.backward()
+            optimizer.step()
             pass
